a2fb78779e43abc4b0dff948436652bce0c3bd20
Alexandru Pascu <alexandru.pascu@totalsoft.ro>
2024-03-11 12:29:20 +0200

    - save progress

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { AccountService } from './_services/account.service';
+import { User } from './_models/user';
 
 @Component({
   selector: 'app-root',
@@ -10,11 +12,26 @@ export class AppComponent implements OnInit {
   title = 'client';
   users: any;
 
-  constructor(private http: HttpClient) {}
+  constructor(
+    private http: HttpClient,
+    private accountService: AccountService
+  ) {}
 
   ngOnInit() {
+    this.getUser();
+    this.setCurrentUser();
+  }
+
+  getUser() {
     this.http
       .get('https://localhost:5001/api/user')
       .subscribe((res) => (this.users = res));
+  }
+
+  setCurrentUser() {
+    const userString = localStorage.getItem('user');
+    if (!userString) return;
+    const user: User = JSON.parse(userString);
+    this.accountService.setCurrentUser(user);
   }
 }
